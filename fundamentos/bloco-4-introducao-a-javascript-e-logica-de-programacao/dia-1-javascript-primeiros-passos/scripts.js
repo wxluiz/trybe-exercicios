7467685b33ebde41229ca3a3942abc55e4b77606
Luiz Henrique <wxluiz@gmail.com>
2022-08-18 19:39:24 -0300

    exercicio 5 e 6 feitos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -55,3 +55,53 @@ else {
     console.log("zero")
 }
 
+
+// 5.Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+let angulo1 = 50
+let angulo2 = 90
+let angulo3 = 30
+let total = angulo1 + angulo2 + angulo3
+
+
+if (angulo1 + angulo2 + angulo3 === 180) {
+    console.log("true")
+}
+else if (typeof total == "string" || typeof total == "boolean") {
+    console.log("ERROR 404")
+}
+else {
+    console.log("false")
+}
+
+
+// 6.Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+
+let pecaDeXadrez = "CAVAlo";
+let minuscula = pecaDeXadrez.toLowerCase();
+
+switch (minuscula) {
+    case "peão":
+        console.log("Move-se sempre uma casa para frente")
+        break
+    case "cavalo":
+        console.log("O movimento executado pelo Cavalo é conhecido por “um-dois” ou “em L”")
+        break
+    case "bispo":
+        console.log("Move-se na diagonal, quantas casas quiser")
+        break
+    case "torre":
+        console.log("Move-se em linha reta, tanto na vertical quanto na horizontal, quantas casas quiser.")
+        break
+    case "dama":
+        console.log("Assim como o Rei, pode mover-se em qualquer direção (vertical, horizontal e diagonal), porém quantas casas quiser, desde que estejam livres.")
+        break
+    case "rei":
+        console.log("Pode mover-se em qualquer direção, porém apenas uma casa por vez, como indicam os pontos vermelhos na figura abaixo.")
+        break
+    default:
+        console.log("ERROR 404")
+}
+
+
+
