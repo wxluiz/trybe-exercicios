ba977a6bf7e81dc265115588c968acf95863f3ff
Luiz Henrique <wxluiz@gmail.com>
2022-08-22 19:18:01 -0300

    com o for criei uma variavel com valor 0 pra ir somando com os index do array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -2,6 +2,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Exercicio 1.
 
+// for (let index = 0; index < numbers.length; index+=1) {
+//     console.log(numbers[index])
+// }
+
+// Exercicio 2
+let sum = 0;
 for (let index = 0; index < numbers.length; index+=1) {
-    console.log(numbers[index])
+   sum = sum + numbers[index]
 }
+console.log(sum)
