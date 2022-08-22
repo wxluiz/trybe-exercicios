ab85ece5275717a3af8b37dc8deca2d50b3bded2
Luiz Henrique <wxluiz@gmail.com>
2022-08-22 19:49:13 -0300

    Exercicio 5 feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -25,10 +25,19 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // Exercicio 4.
 
 
-let sum = 0;
-for (let index = 0; index < numbers.length; index+=1) {
-   sum = sum + numbers[index]
-   media = sum / numbers.length
+// let sum = 0;
+// for (let index = 0; index < numbers.length; index+=1) {
+//    sum = sum + numbers[index]
+//    media = sum / numbers.length
+// }
+// console.log(media > 20? "Valor maior que 20":"valor menor ou igual a 20")
+
+// Exercicio 5.
+let maiorNumero = numbers[0]
+for (index = 0; index < numbers.length; index +=1) {
+    if (numbers[index] > maiorNumero) {
+        maiorNumero = numbers[index]
+    }
 }
-console.log(media > 20? "Valor maior que 20":"valor menor ou igual a 20")
+console.log(maiorNumero)
 
