d9e08324ba5b13001643fefb470344c678a1b70d
Luiz Henrique <wxluiz@gmail.com>
2022-08-22 19:21:41 -0300

    exercicio 3 feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -7,8 +7,18 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // Exercicio 2
+// let sum = 0;
+// for (let index = 0; index < numbers.length; index+=1) {
+//    sum = sum + numbers[index]
+// }
+// console.log(sum)
+
+// Exercicio 3 
+
 let sum = 0;
 for (let index = 0; index < numbers.length; index+=1) {
    sum = sum + numbers[index]
+   media = sum / numbers.length
 }
-console.log(sum)
+console.log(media)
+
