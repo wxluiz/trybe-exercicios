461f7e711663be87e76fbb6d06615a86cee17675
Luiz Henrique <wxluiz@gmail.com>
2022-08-23 20:38:46 -0300

    Exercicio 2 feito, inverti uma palavra com o comando for

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -2,8 +2,16 @@
 
 let fatorial = 10;
 let resultado = 1;
-for (index = fatorial; index > 0; index -=1) {
+for (let index = fatorial; index > 0; index -=1) {
     resultado *= index
 }
     console.log("!" + fatorial + " = " + resultado)
 
+// Exercicio 2.
+
+let word = 'tryber';
+let invert = ""
+for (let index = word.length -1; index >= 0; index -=1) {
+    invert += word[index]
+}
+console.log(invert)
