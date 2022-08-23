2b20e45ecd866412a34425648297bfc9dc28f4e8
Luiz Henrique <wxluiz@gmail.com>
2022-08-23 20:30:27 -0300

    Exercicio 1 feito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -0,0 +1,9 @@
+// Exercicio 1.
+
+let fatorial = 10;
+let resultado = 1;
+for (index = fatorial; index > 0; index -=1) {
+    resultado *= index
+}
+    console.log("!" + fatorial + " = " + resultado)
+
