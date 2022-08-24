59286b7b9f11ee2bcd5624ffc8fffcf03590b448
Luiz Henrique <wxluiz@gmail.com>
2022-08-24 12:55:30 -0300

    Exercicio 5 corrigido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -7,47 +7,29 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
-  console.log("Bem vinda, " + info.personagem);
+//   console.log("Bem vinda, " + info.personagem);
 
 
 //   2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. 
 
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
-  };
+info.recorrente = 'Sim';
+// console.log(info)
 
-console.log(info)
 
 // 3 - Faça um for/in que mostre todas as chaves do objeto. 
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
-  };
 
-for (let key in info) {
-    console.log(key);
-}
+// for (let key in info) {
+    // console.log(key);
+// }
 
 // 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. 
 
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
-  };
-
-for (let key in info) {
-    console.log(info[key]);
-}
+// for (let key in info) {
+//     console.log(info[key]);
+// }
 
 
 // 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. 
@@ -59,9 +41,13 @@ let info2 = {
     recorrente: 'Sim'
   };
 
-  for (let key in info2) {
-    console.log(key + ": " + info2[key]);
+  for (let key in info) {
+    if (key === "recorrente" && info[key] === "Sim" && info2[key] === "Sim") {
+        console.log("Ambos recorrentes");
+    } else {
+        console.log(info[key] + " e " + info2[key])
+    }
+    
   }
 
 
-  
