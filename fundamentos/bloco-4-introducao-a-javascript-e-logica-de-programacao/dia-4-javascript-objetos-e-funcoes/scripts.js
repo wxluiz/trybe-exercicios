7ce0f0387fa7e7258431392361a63ec06e9087f1
Luiz Henrique <wxluiz@gmail.com>
2022-08-24 12:12:18 -0300

    Exercicio 5 feito, criei novo objeto e imprimi as chaves e os valores

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -1,4 +1,4 @@
-// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 
 
 let info = {
@@ -10,7 +10,7 @@ let info = {
   console.log("Bem vinda, " + info.personagem);
 
 
-//   2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+//   2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. 
 
 
 let info = {
@@ -22,7 +22,7 @@ let info = {
 
 console.log(info)
 
-// 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+// 3 - Faça um for/in que mostre todas as chaves do objeto. 
 
 let info = {
     personagem: 'Margarida',
@@ -35,7 +35,7 @@ for (let key in info) {
     console.log(key);
 }
 
-// 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+// 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. 
 
 
 let info = {
@@ -50,5 +50,18 @@ for (let key in info) {
 }
 
 
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. 
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  };
+
+  for (let key in info2) {
+    console.log(key + ": " + info2[key]);
+  }
 
 
+  
