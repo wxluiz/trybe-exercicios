8d2a0e620002eee1258e0696f4e35ce16f3f5af6
Luiz Henrique <wxluiz@gmail.com>
2022-08-24 13:06:25 -0300

    Exercicio 6 feito, imprimi a frase pedida concatenando os valores do objeto

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -1,11 +1,11 @@
 // 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
 //   console.log("Bem vinda, " + info.personagem);
 
@@ -13,7 +13,7 @@ let info = {
 //   2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. 
 
 
-info.recorrente = 'Sim';
+// info.recorrente = 'Sim';
 // console.log(info)
 
 
@@ -34,20 +34,42 @@ info.recorrente = 'Sim';
 
 // 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. 
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim'
+//   };
+
+//   for (let key in info) {
+//     if (key === "recorrente" && info[key] === "Sim" && info2[key] === "Sim") {
+//         console.log("Ambos recorrentes");
+//     } else {
+//         console.log(info[key] + " e " + info2[key])
+//     }
+    
+//   }
+
+
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+
+  let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
 
-  for (let key in info) {
-    if (key === "recorrente" && info[key] === "Sim" && info2[key] === "Sim") {
-        console.log("Ambos recorrentes");
-    } else {
-        console.log(info[key] + " e " + info2[key])
-    }
-    
-  }
+
+//   6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+console.log("O livro favorito de " + leitor["nome"] + " " + leitor["sobrenome"] + " se chama " + leitor["livrosFavoritos"][0]["titulo"])
 
 
