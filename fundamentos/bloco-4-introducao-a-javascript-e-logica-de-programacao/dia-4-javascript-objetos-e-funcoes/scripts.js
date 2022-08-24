3c11ab599da89a5f94e9741487beb8f2a248218f
Luiz Henrique <wxluiz@gmail.com>
2022-08-24 12:02:00 -0300

    Exercicio 3 feito, executei for in para imprimir somente as chaves

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -22,5 +22,19 @@ let info = {
 
 console.log(info)
 
+// 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+
+for (let key in info) {
+    console.log(key);
+}
+
+
 
 
