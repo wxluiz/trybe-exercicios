723fa9db7721fbacfc421c51ba6fb40488611169
Luiz Henrique <wxluiz@gmail.com>
2022-08-23 22:45:18 -0300

    Exercicio 3 feito, utilizei for para mostrar a maior e a menor palavra em um array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -15,3 +15,21 @@ for (let index = word.length -1; index >= 0; index -=1) {
     invert += word[index]
 }
 console.log(invert)
+
+
+// Exercicio 3.
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = array[0];
+let menor = array[0];
+for (let index = 0; index < array.length; index +=1) {
+    if (array[index].length > maior.length) {
+        maior = array[index]
+    }
+    else if (array[index].length < menor.length) {
+        menor = array[index]
+    }
+}
+console.log(maior)
+console.log(menor)
+
