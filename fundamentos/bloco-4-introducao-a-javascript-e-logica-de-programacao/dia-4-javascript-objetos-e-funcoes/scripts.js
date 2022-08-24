369aa228849bc398488b49d812eee7f6d5df9bce
Luiz Henrique <wxluiz@gmail.com>
2022-08-24 11:59:33 -0300

    Exercicio 2 feito, adicionei uma chave e valor no objeto e imprimi o objeto

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -1,3 +1,6 @@
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+
+
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
@@ -5,3 +8,19 @@ let info = {
   };
 
   console.log("Bem vinda, " + info.personagem);
+
+
+//   2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+
+console.log(info)
+
+
+
