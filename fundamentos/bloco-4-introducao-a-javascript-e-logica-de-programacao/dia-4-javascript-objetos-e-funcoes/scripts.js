b28896669268ea66b69dba637f3697f6a1c88f9c
Luiz Henrique <wxluiz@gmail.com>
2022-08-25 18:43:34 -0300

    Exercicio 2 de funções feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -93,16 +93,34 @@
 
 // 1 - Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
 
-function verificarPalindromo (palavra) {
-    let reverse = palavra.split('').reverse().join('');
-    if (reverse === palavra) {
-        return true
-    }
-    else {
-        return false
-    }
-}
+// function verificarPalindromo (palavra) {
+//     let reverse = palavra.split('').reverse().join('');
+//     if (reverse === palavra) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
 
-console.log(verificarPalindromo("arara"))
-console.log(verificarPalindromo("paralelepipedo"))
+// console.log(verificarPalindromo("arara"))
+// console.log(verificarPalindromo("paralelepipedo"))
+
+
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function maiorValor (array) {
+    let numero1 = array[0];
+    let posicao = 0;
+    for (let index in array) {
+        if (numero1 < array[index]) {
+            numero1 = array[index];
+            posicao = index;
+        }
+    }
+    return posicao
+}
+console.log(maiorValor([2, 3, 6, 7, 10, 1]))
+
+
 
