ecbc851c266038462229952ab41111fa84daec73
Luiz Henrique <wxluiz@gmail.com>
2022-09-08 18:57:46 -0300

    Exercicio bonus concluido

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,7 +1,42 @@
 function interrupt(event) {
     event.preventDefault();
+    const validacao = textInputValidation();
+    if (validacao === false) {
+        alert('Dados Inválidos');
+    }
+    else {
+        alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+    }
   }
   window.onload = function () {
     const btnSubmit = document.querySelector('#btn-submit');
     btnSubmit.addEventListener('click', interrupt);
+    const checkbox = document.querySelector('#autoriza');
+    checkbox.addEventListener('change', enableButton);
   };
+
+  function enableButton() {
+    const btnSubmit = document.querySelector('#btn-submit');
+    const checkbox = document.querySelector('#autoriza');
+    btnSubmit.disabled = !checkbox.checked;
+  }
+
+
+
+function textInputValidation() {
+    const email = document.querySelector('#email').value.length;
+    const invalidEmail = email < 10 || email > 50;
+  
+    const name = document.querySelector('#nomeCompleto').value.length;
+    const invalidName = name < 10 || name > 40;
+  
+    const reason = document.querySelector('#motivo').value.length;
+    const invalidReason = reason > 500;
+  
+    if (invalidEmail || invalidName || invalidReason) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
