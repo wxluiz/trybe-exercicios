46f2df35bf77de2955111fa4fd0cf4db210dbae9
Luiz Henrique <wxluiz@gmail.com>
2022-09-20 13:44:48 -0300

    Exercicios concluidos

--- fundamentos/secao-7-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/dia-3-Primeiros-passos-em-Jest/sum.js
+++ fundamentos/secao-7-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/dia-3-Primeiros-passos-em-Jest/sum.js
@@ -4,9 +4,9 @@ function sum(a, b) {
     }
   
     return a + b;
-  }
+}
 
-  function myRemove(arr, item) {
+function myRemove(arr, item) {
     let newArr = [];
     for (let index = 0; index < arr.length; index += 1) {
       if (item !== arr[index]) {
@@ -14,6 +14,80 @@ function sum(a, b) {
       }
     }
     return newArr;
-  }
+}
 
-  module.exports = { sum, myRemove };
+function myFizzBuzz(num) {
+    if (typeof num !== 'number') return false;
+    if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+    if (num % 3 === 0) return 'fizz';
+    if (num % 5 === 0) return 'buzz';
+    return num;
+}
+
+const vogais = ['a', 'e', 'i', 'o', 'u'];
+const code = [1, 2, 3, 4, 5];
+
+function encode(text) {
+  const caracters = text.split('');
+  caracters.forEach((letra, i) => {
+    vogais.forEach((vogal, k) => {
+      caracters[i] = (letra === vogal) ? code[k] : caracters[i];
+    });
+  });
+
+  return caracters.join('');
+}
+
+function decode(text) {
+  const caracters = text.split('');
+  caracters.forEach((caracter, i) => {
+    code.forEach((num, k) => {
+      caracters[i] = (caracter === num.toString()) ? vogais[k] : caracters[i];
+    });
+  });
+
+  return caracters.join('');
+}
+
+
+const techList = (arrayTechnologies, name) => {
+    if (arrayTechnologies.length === 0) return 'Vazio!';
+  
+    const sortedArray = arrayTechnologies.sort();
+    const technologyList = [];
+  
+    for (let index = 0; index < sortedArray.length; index += 1) {
+      technologyList.push({
+        tech: sortedArray[index],
+        name,
+      });
+    }
+  
+    return technologyList;
+  };
+
+  
+const hydrate = (string) => {
+    const splitString = string.split('');
+    let glassesOfWater = 0;
+  
+    for (let index = 0; index < splitString.length; index += 1) {
+      const parsedCharacter = parseInt(splitString[index]);
+  
+      if (parsedCharacter) {
+        glassesOfWater += parsedCharacter;
+      }
+    }
+  
+    let glass = 'copo';
+  
+    if (glassesOfWater > 1) {
+      glass = 'copos';
+    }
+  
+    return `${glassesOfWater} ${glass} de água`;
+  };
+  
+
+
+  module.exports = { sum, myRemove, myFizzBuzz, encode, decode, techList, hydrate };
--- fundamentos/secao-7-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/dia-3-Primeiros-passos-em-Jest/sum.test.js
+++ fundamentos/secao-7-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/dia-3-Primeiros-passos-em-Jest/sum.test.js
@@ -35,8 +35,114 @@ describe('Testes Exercicio 2', () => {
         expect((myRemove([1, 2, 3, 4], 5))).toEqual([1, 2, 3, 4]);
     });   
 })
+
+// myFizzBuzz.test.js
+const { myFizzBuzz } = require('./sum');
+
+describe('Testes do Exercicio 3', () => {
+  it('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+    })
+
+    it('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(3)).toBe('fizz');
+    });  
+
+    it('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(5)).toBe('buzz');
+    });  
+
+    it('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(1)).toBe(1);
+    });  
+
+    it('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz('2')).toBe(false);
+    });   
+})
+
+const {encode, decode} = require('./sum');
+
+describe('Testes Exercicio 4', () => {
+
+    it('Teste se encode e decode são funções;', () => {
+        expect((typeof encode)).toEqual('function');
+        expect((typeof decode)).toEqual('function');
+    });  
+    
+    it('Para a função encode, teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente;', () => {
+        expect((encode('aeiou'))).toEqual('12345');
+    });  
+
+    it('Para a função decode, teste se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente;', () => {
+        expect((decode('12345'))).toEqual('aeiou');
+    });  
+
+    it('Teste se as demais letras/números não são convertidos para cada caso;', () => {
+        expect(encode('ovo')).toEqual('4v4');
+        expect(decode('1n1')).toEqual('ana');
+    });  
+
+    it('Teste se a string que é retornada pelas funções tem o mesmo número de caracteres que a string passada como parâmetro.', () => {
+        expect(encode('trybe').length).toEqual(5);
+        expect(decode('trybe').length).toEqual(5);
+    });  
+})
   
 
-// it('', () => {
-//     expect()).toBe();
-// });   
+const {techList} = require('./sum');
+
+describe('Testa a função techList', () => {
+  it('Testa se a função techList é definida', () => {
+    expect(techList).toBeDefined();
+  });
+  it('Testa se techList é uma função', () => {
+    expect(typeof techList).toBe('function');
+  });
+  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+    expect(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')).toEqual([
+      {
+        tech: 'CSS',
+        name: 'Lucas'
+      },
+      {
+        tech: 'HTML',
+        name: 'Lucas'
+      },
+      {
+        tech: 'JavaScript',
+        name: 'Lucas'
+      },
+      {
+        tech: 'Jest',
+        name: 'Lucas'
+      },
+      {
+        tech: 'React',
+        name: 'Lucas'
+      }
+    ]);
+  });
+  it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+    expect(techList([], 'Lucas')).toBe('Vazio!');
+  });
+});
+
+const {hydrate} = require('./sum');
+
+describe('Testa a função hydrate', () => {
+  it('Testa se a função hydrate é definida', () => {
+    expect(hydrate).toBeDefined();
+  });
+  it('Testa se hydrate é uma função', () => {
+    expect(typeof hydrate).toBe('function');
+  });
+  it('Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber', () => {
+    expect(hydrate('1 cerveja')).toBe('1 copo de água');
+    expect(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho')).toBe('7 copos de água');
+    expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
+    expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe('3 copos de água');
+    expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
+  });
+});
+
