8a1be48f75bca1068c2ff9ef1797dfdaa5c0893a
Luiz Henrique <wxluiz@gmail.com>
2022-09-22 20:16:27 -0300

    ajustes finais

--- fundamentos/secao-8-Higher-Order-Functions-do-JavaScript-ES6/dia-1-Introducao-a-Higher-Order-Functions/script.js
+++ fundamentos/secao-8-Higher-Order-Functions-do-JavaScript-ES6/dia-1-Introducao-a-Higher-Order-Functions/script.js
@@ -1,33 +1,33 @@
 // Exercicio 1
 
-// const emails = (name) => {
-//    return { 'nome': name, Email: `${name.replace(' ', '_').toLowerCase()}@trybe.com` };
-// }
+const emails = (name) => {
+   return { 'nome': name, Email: `${name.replace(' ', '_').toLowerCase()}@trybe.com` };
+}
 
-// const newEmployees = (callback) => {
-//     const employees = {
-//       id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-//       id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-//       id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-//     }
-//     return employees;
-//   };
+const newEmployees = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
 
-//   console.log(newEmployees(emails))
+  console.log(newEmployees(emails))
 
 // Exercicio 2
 
-// const luckNumber = () => Math.ceil(Math.random() * 5)
+const luckNumber = () => Math.ceil(Math.random() * 5)
 
-// const winnerNumber = (number, callback) => {
-//     const numero = callback();
-//     if (number === numero) {
-//         return 'Parabéns você ganhou';
-//     }
-//     return `numero sorteado: ${numero}, Tente novamente`;
-// }
+const winnerNumber = (number, callback) => {
+    const numero = callback();
+    if (number === numero) {
+        return 'Parabéns você ganhou';
+    }
+    return `numero sorteado: ${numero}, Tente novamente`;
+}
 
-// console.log(winnerNumber(4, luckNumber))
+console.log(winnerNumber(4, luckNumber))
 
 // Exercicio 3
 
