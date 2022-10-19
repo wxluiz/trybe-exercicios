d1edae25768b6ad82bd278a0a7b9179f97673940
Luiz Henrique <wxluiz@gmail.com>
2022-10-19 01:27:38 -0300

    Exercicio 8 concluido

---
+++ fundamentos/secao-8-Higher-Order-Functions-do-JavaScript-ES6/dia-5-JavaScript-ES6-spread-operator-parâmetro-rest-destructuring-e-mais/exercise8.js
@@ -0,0 +1,10 @@
+// escreva greet abaixo
+
+const greet = (name, greet = 'Hi') => {
+  return `${greet} ${name}`
+}
+
+// Retornos esperados:
+console.log(greet('John')); // 'Hi John'
+console.log(greet('John', 'Good morning')); // 'Good morning John'
+console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
